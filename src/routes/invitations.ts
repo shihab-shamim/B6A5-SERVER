c9f3ffc45c33ab4d63af2d552592f9992285d6a4
shihab-shamim <shihabshamim767@gmail.com>
2026-03-31 19:46:58 +0600

    all complete

--- src/routes/invitations.ts
+++ src/routes/invitations.ts
@@ -3,7 +3,7 @@ const router = express.Router();
 import {  getMyInvitations, updateInvitation  } from "../controllers/invitationController";
 import {  protect  } from "../middleware/authMiddleware";
 
-// Protected routes
+
 router.get("/", protect, getMyInvitations);
 router.put("/:id", protect, updateInvitation);
 
