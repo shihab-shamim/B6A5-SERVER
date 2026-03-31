c6485cf45090b4724f895bfb812b619354e305ae
shihab-shamim <shihabshamim767@gmail.com>
2026-03-31 19:45:06 +0600

    admin routes

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -3,12 +3,14 @@ const router = express.Router();
 import {  getUsers, deleteUser, getAllEventsAdmin  } from "../controllers/adminController";
 import {  protect, admin  } from "../middleware/authMiddleware";
 
-// All admin routes must be protected and restricted to admin
+
 router.use(protect, admin);
 
 router.get("/users", getUsers);
 router.delete("/users/:id", deleteUser);
 router.get("/events", getAllEventsAdmin);
-// Note: delete event by admin is handled in eventController.js (`DELETE /api/events/:id`)
+
 
 export default router;
+
+
